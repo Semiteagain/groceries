e8821587300252ca90237aae89aad222bfa055f9
Semiu Mukaila <semiteagain@gmail.com>
2023-09-07 18:32:53 +0100

    adding icons

--- lib/constants/intro_page_constant.dart
+++ lib/constants/intro_page_constant.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 
 class IntroConstants {
-  static const Color backgroundColor = Color.fromARGB(255, 229, 252, 249);
+  static const Color backgroundColor = Color.fromARGB(255, 6, 143, 125);
+  static const String introShop = "Fruit Shop";
   static const String introTitle = "Groceries";
+  static const String introSubtitle =
+      "Buy food or groceries online easly only with mobile phone";
+  static const String introImage = 'lib/images/vegetable.png';
 }
--- lib/pages/intro_page.dart
+++ lib/pages/intro_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:groceries/components/button.dart';
 import 'package:groceries/constants/intro_page_constant.dart';
 
 class IntroPage extends StatelessWidget {
@@ -7,33 +8,67 @@ class IntroPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       backgroundColor: (IntroConstants.backgroundColor),
       body: Padding(
-        padding: EdgeInsets.all(25.0),
-        child: Column(children: [
-          SizedBox(
-            height: 25,
-          ),
-          Text(
-            IntroConstants.introTitle,
-          ),
-          SizedBox(
-            height: 10,
-          ),
-          // subtittle
-          Text(
-            "Buy food or groceries online easly only with mobile phone",
-            style: TextStyle(
-              color: Colors.grey,
-              height: 2,
+        padding: const EdgeInsets.all(25.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            const SizedBox(
+              height: 25,
             ),
-          ),
-          
-          SizedBox(
-            height: ,
-          )
-        ]),
+
+            Text(
+              IntroConstants.introShop,
+              style: GoogleFonts.dmSerifDisplay(
+                fontSize: 28,
+                color: Colors.white,
+              ),
+            ),
+
+            const SizedBox(
+              height: 25,
+            ),
+
+            Padding(
+              padding: const EdgeInsets.all(50.0),
+              child: Image.asset(IntroConstants.introImage),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+
+            Text(
+              IntroConstants.introTitle,
+              style: GoogleFonts.dmSerifDisplay(
+                fontSize: 44,
+                color: Colors.white,
+              ),
+            ),
+
+            // subtittle
+            const Text(
+              IntroConstants.introSubtitle,
+              style: TextStyle(
+                color: Color.fromARGB(255, 235, 201, 201),
+                height: 2,
+              ),
+            ),
+
+            const SizedBox(
+              height: 25,
+            ),
+
+            MyButton(
+              text: "Get Started",
+              onTap: () {
+                Navigator.pushNamed(context, "/menupage");
+              },
+            ),
+          ],
+        ),
       ),
     );
   }

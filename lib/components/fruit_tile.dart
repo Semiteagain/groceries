8d6d564f8ed0383702b8a88064a72c0bb7b33d76
Semiu Mukaila <semiteagain@gmail.com>
2023-09-15 13:25:03 +0100

    singlesrow widget

--- lib/components/fruit_tile.dart
+++ lib/components/fruit_tile.dart
@@ -18,47 +18,48 @@ class FruitTile extends StatelessWidget {
       ),
       margin: const EdgeInsets.only(left: 25),
       padding: const EdgeInsets.all(25),
-      child: Column(
-        // crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Image.asset(
-            fruit.imgPath,
-            height: 40,
-          ),
-          Text(
-            fruit.name,
-            style: GoogleFonts.dmSerifDisplay(fontSize: 15),
-          ),
-          SizedBox(
-            width: 160,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  '\$' + fruit.price,
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    color: Colors.grey[700],
-                  ),
-                ),
-                Row(
-                  children: [
-                    Icon(
-                      Icons.star,
-                      color: Colors.yellow[900],
-                    ),
-                    Text(
-                      fruit.rating,
-                      style: const TextStyle(
-                        color: Colors.grey,
-                      ),
-                    ),
-                  ],
-                ),
-              ],
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            Image.asset(
+              fruit.imgPath,
+              height: 40,
             ),
-          )
-        ],
+            Text(
+              fruit.name,
+              style: GoogleFonts.dmSerifDisplay(fontSize: 15),
+            ),
+            SizedBox(
+              width: 160,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    '\$' + fruit.price,
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.grey[700],
+                    ),
+                  ),
+                  Row(
+                    children: [
+                      Icon(
+                        Icons.star,
+                        color: Colors.yellow[900],
+                      ),
+                      Text(
+                        fruit.rating,
+                        style: const TextStyle(
+                          color: Colors.grey,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }

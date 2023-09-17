eb997266872187134c942ca7e8fed6fbe2d895b2
Semiu Mukaila <semiteagain@gmail.com>
2023-09-17 19:47:51 +0100

    onTap

--- lib/components/fruit_tile.dart
+++ lib/components/fruit_tile.dart
@@ -4,10 +4,8 @@ import 'package:groceries/models/fruits.dart';
 
 class FruitTile extends StatelessWidget {
   final Fruit fruit;
-  const FruitTile({
-    super.key,
-    required this.fruit,
-  });
+  final void Function()? onTap;
+  const FruitTile({super.key, required this.fruit, required this.onTap});
 
   @override
   Widget build(BuildContext context) {
@@ -17,48 +15,51 @@ class FruitTile extends StatelessWidget {
         borderRadius: BorderRadius.circular(20),
       ),
       margin: const EdgeInsets.only(left: 25),
-      padding: const EdgeInsets.all(25),
+      padding: const EdgeInsets.all(20),
       child: SingleChildScrollView(
-        child: Column(
-          children: [
-            Image.asset(
-              fruit.imgPath,
-              height: 40,
-            ),
-            Text(
-              fruit.name,
-              style: GoogleFonts.dmSerifDisplay(fontSize: 15),
-            ),
-            SizedBox(
-              width: 160,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    '\$' + fruit.price,
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      color: Colors.grey[700],
-                    ),
-                  ),
-                  Row(
-                    children: [
-                      Icon(
-                        Icons.star,
-                        color: Colors.yellow[900],
-                      ),
-                      Text(
-                        fruit.rating,
-                        style: const TextStyle(
-                          color: Colors.grey,
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
+        child: GestureDetector(
+          onTap: onTap,
+          child: Column(
+            children: [
+              Image.asset(
+                fruit.imgPath,
+                height: 140,
               ),
-            )
-          ],
+              Text(
+                fruit.name,
+                style: GoogleFonts.dmSerifDisplay(fontSize: 15),
+              ),
+              SizedBox(
+                width: 160,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      '\$' + fruit.price,
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.grey[700],
+                      ),
+                    ),
+                    Row(
+                      children: [
+                        Icon(
+                          Icons.star,
+                          color: Colors.yellow[900],
+                        ),
+                        Text(
+                          fruit.rating,
+                          style: const TextStyle(
+                            color: Colors.grey,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );

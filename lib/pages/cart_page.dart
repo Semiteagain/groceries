7a44272012cdb425073f307c9fd94280724cadc5
Semiu Mukaila <semiteagain@gmail.com>
2023-09-21 14:38:23 +0100

    cart

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:groceries/components/button.dart';
 import 'package:groceries/models/fruits.dart';
 import 'package:groceries/models/shop.dart';
 import 'package:groceries/theme/colors.dart';
@@ -7,28 +8,73 @@ import 'package:provider/provider.dart';
 class CartPage extends StatelessWidget {
   const CartPage({super.key});
 
+  void removeFromCart(Fruit fruit, BuildContext context) {
+    final shop = context.read<Shop>();
+    shop.removeFromCart(fruit);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Consumer<Shop>(
       builder: (context, value, child) => Scaffold(
+        backgroundColor: primaryColor,
         appBar: AppBar(
           title: const Text('My Cart'),
           backgroundColor: primaryColor,
+          elevation: 0,
         ),
-        body: ListView.builder(
-          itemCount: value.cart.length,
-          itemBuilder: (context, index) {
-            final Fruit fruit = value.cart[index];
+        body: Column(
+          children: [
+            Expanded(
+              child: ListView.builder(
+                itemCount: value.cart.length,
+                itemBuilder: (context, index) {
+                  final Fruit fruit = value.cart[index];
 
-            final String fruitName = fruit.name;
+                  final String fruitName = fruit.name;
 
-            final String fruitPrice = fruit.price;
+                  final String fruitPrice = fruit.price;
 
-            return ListTile(
-              title: Text(fruitName),
-              subtitle: Text(fruitPrice),
-            );
-          },
+                  return Container(
+                    decoration: BoxDecoration(
+                      color: secondaryColor,
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                    margin: const EdgeInsets.only(left: 20, top: 20, right: 20),
+                    child: ListTile(
+                      title: Text(
+                        fruitName,
+                        style: const TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      subtitle: Text(
+                        fruitPrice,
+                        style: TextStyle(
+                          color: Colors.grey[200],
+                        ),
+                      ),
+                      trailing: IconButton(
+                        icon: Icon(
+                          Icons.delete,
+                          color: Colors.grey[300],
+                        ),
+                        onPressed: () => removeFromCart(fruit, context),
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(25.0),
+              child: MyButton(
+                text: 'Pay Now',
+                onTap: () {},
+              ),
+            ),
+          ],
         ),
       ),
     );
